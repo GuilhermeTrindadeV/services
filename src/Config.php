3e2706560f0e496bbe3d7a5f35889c903cb9bc8a
GuilhermeTrindadeV <guiviveiro@gmail.com>
2023-01-31 21:38:52 -0300

    v0.1.0

--- src/Config.php
+++ src/Config.php
@@ -1,13 +1,14 @@
 <?php
 
-define("ROOT", "http://localhost");
+define("ENV", parse_ini_file(realpath(dirname(__FILE__, 2) . "/env.ini")));
+define("ROOT", ENV["app_url"]);
 define("DATA_LAYER", [
-    "driver" => "mysql",
-    "host" => "localhost",
-    "port" => "3308",
-    "dbname" => "services",
-    "username" => "root",
-    "passwd" => "root",
+    "driver" => ENV["db_driver"],
+    "host" => ENV["db_host"],
+    "port" => ENV["db_port"],
+    "dbname" => ENV["db_name"],
+    "username" => ENV["db_username"],
+    "passwd" => ENV["db_passwd"],
     "options" => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -15,14 +16,14 @@ define("DATA_LAYER", [
         PDO::ATTR_CASE => PDO::CASE_NATURAL
     ]
 ]);
-define("SITE_NAME", "Services");
-define("SESS_NAME", "user_se");
-define("MESSAGE_NAME", "message");
+define("SITE_NAME", ENV["app_name"]);
+define("SESS_NAME", ENV["app_sessname"]);
+define("MESSAGE_NAME", ENV["app_messagename"]);
 define('MAIL', [
-    'host' => 'mail.hospedasmart.com.br',
-    'port' => '465',
-    'username' => '_mainaccount@hospedasmart.com.br',
-    'password' => 'hospeda@net12',
-    'name' => 'GTGNG - Services',
-    'email' => 'contato@gtgng.software'
+    'host' => ENV["mail_host"],
+    'port' => ENV["mail_port"],
+    'username' => ENV["mail_username"],
+    'password' => ENV["mail_passwd"],
+    'name' => ENV["mail_password"],
+    'email' => ENV["mail_email"]
 ]);
